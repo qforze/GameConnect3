b575390d55d65c1229a0ff8dd65b204930c0b8c6
Laurent <laurentclevers@gmail.com>
2019-02-21 20:21:38 +0200

    Fixed an issue where the game wouldn't finish in case of a tie

--- app/src/main/java/com/loloc/gameconnect3/MainActivity.java
+++ app/src/main/java/com/loloc/gameconnect3/MainActivity.java
@@ -24,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
     boolean gameIsActive = true;
 
-    // 2 means unplayed
-
    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
